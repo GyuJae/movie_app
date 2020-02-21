aa608fc6b08a9c28d79d15629ac461ebca5ece11
gyu jae <wjdrbwo1206@naver.com>
2020-02-22 02:11:17 +0900

    Updates

---
+++ precache-manifest.3fa4cbaa14b856ae26955c6c654fcc35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b326f1aa9b38f60696003f803d9e451",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5a53f0b09139b4730338",
+    "url": "/movie_app/static/css/main.de8cc2e2.chunk.css"
+  },
+  {
+    "revision": "cc1cf73e3dddd4bc9ea3",
+    "url": "/movie_app/static/js/2.e7f4c5fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.e7f4c5fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a53f0b09139b4730338",
+    "url": "/movie_app/static/js/main.152ee4d3.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
